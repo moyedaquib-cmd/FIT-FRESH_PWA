2df1e10fc920ca4ca454f14d302e0bf3e72de060
moyedaquib-cmd <moyedaquib@gmail.com>
2026-01-27 18:27:31 +1100

    Added the login function meaning that users can now log in to their accounts. Details about the user are also saved.

--- App.py
+++ App.py
@@ -1,13 +1,13 @@
 import os #Allows python to interact with the operating system
-from flask import Flask, request, redirect, url_for, render_template #Imports flask which I will use to create my PWA, request which reads data sent from forms
+from flask import Flask, request, redirect, url_for, render_template, session #Imports flask which I will use to create my PWA, request which reads data sent from forms
 from DB_Models import db, User #Imports the database controller object I created and the user table
-from werkzeug.security import generate_password_hash #Allows plaintext passwords to be stored as hashed values
+from werkzeug.security import generate_password_hash, check_password_hash #Allows plaintext passwords to be stored as hashed values and checks between hashed passwords and plaintext passwords to match them, render_template to render HTML files from the templates directory, session to remember information like login status
 app = Flask(__name__) #This line creates the flask application
 basedir = os.path.abspath(os.path.dirname(__file__)) #This line of code is used to turn the path into a fully resolved absolute path so that the program works on all operating systems. "__file__" relates to the full path of the file being executed. "os.path.dirname(__file__)" removes the file name 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "instance", "app.db") #This line builds the file path while telling flask exactly where the database is located. "SQLite:///" informs Flask to use SQLite and create an absolute path. "os.path.join" builds the file path. This makes it so that the program works with any operating systems and resolves correctly every time.
 app.config["SECRET_KEY"] = "oJvneTznic84TgELjsKA" #This is a secret key used by flask to lock the login sessions so that people with no knowledge of the key cant access important information and tamper with cookies.
 db.init_app(app) #Connects the database to the app
-@app.route("/register", methods=["GET"])
+@app.route("/register", methods=["GET"]) #Returns the actual register page where the user can view and register an account
 def register_page():
     return render_template("register.html")
 @app.route("/register", methods=["POST"]) #This url is for the user to register an account. It aonly accepts POST requests as it handles sensitive data (passwords)
@@ -25,6 +25,22 @@ def register(): #This function runs when "/register" receives a POST request
     db.session.add(new_user) #These lines save the program to the database "add()" stages the object while "Commit()" permanently saves it
     db.session.commit() #Confirms the user has successfully registered
     return "Successfully registered"
+@app.route("/login", methods=["POST", "GET"]) #This function runs when "/login" receives a POST request
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if not username or not password:
+        return "Please fill in all the fields", 400
+    user =  User.query.filter_by(username=username).first()
+    if not user: #If no account exists with the username filled in, the PWA doesn't allows them to log in
+        return "Invalid username or password", 401 #Invalid username or passwird increases safety as attackers dont know what is wrong with their login form
+    if not check_password_hash(user.password_hash, password): #Checks the hashed password with the user table and compares the plaintext and hashed versions to see if the password is correct
+        return "Invalid username or password", 401
+    session["user_id"] = user.id #Stores the user's unique ID allowing easy identification of the logged-in user
+    session["role"] = user.role #Stores the role of the user to enable role-based access control
+    return redirect(url_for("home"))
 @app.route("/")
 def home():
     return "RUNNING"
